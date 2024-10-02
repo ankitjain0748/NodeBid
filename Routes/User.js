de276f468565678468bfbf9104577e9df016b2c2
ankitjain0748 <ankitjain0748@gmail.com>
2024-10-02 20:35:56 +0530

    first commit

--- Routes/User.js
+++ Routes/User.js
@@ -3,7 +3,7 @@ const router =  require("express").Router();
 
 const { validateToken, signup, login, getotpsingup,userlist,user } = require("../Controller/UserController")
 
-router.post("/singup", signup);
+router.post("/signup", signup);
 router.post("/getotp", getotpsingup);
 router.post("/login", login);
 router.get("/list", userlist);
